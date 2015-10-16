4e7d3c9c7d354b989b20270fbed07131546a996f
Alex Curtis <alex.curtis@uswitch.com>
2015-10-16 15:08:25 +0100

    Rename buttonWasPressed

--- ThemePicker.elm
+++ ThemePicker.elm
@@ -5,11 +5,11 @@ import Html.Events exposing (..)
 
 
 type alias Model =
-  { buttonWasPressed : Bool }
+  { itemWasSelected : Bool }
 
 
 initialModel : Model
-initialModel = { buttonWasPressed = False }
+initialModel = { itemWasSelected = False }
 
 
 type Action
@@ -20,15 +20,15 @@ type Action
 update : Action -> Model -> Model
 update action model =
   case action of
-    ItemSelected i -> {model | buttonWasPressed <- True }
+    ItemSelected i -> { model | itemWasSelected <- True }
 
 
 view : Signal.Address Action -> Model -> Html
 view address model =
   div [ ] [ if
-              model.buttonWasPressed == True
+              model.itemWasSelected == True
             then
-              p [ ] [ text "Button was pressed!"]
+              p [ ] [ text "Theme selected!"]
             else
               text "" -- TODO: Is there something like Html.Nothing to use here?
           , button
